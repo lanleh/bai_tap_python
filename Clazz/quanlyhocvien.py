ec7742f18b82da20be396a4417b4c3c80792e44e
lanleh <134289739+lanleh@users.noreply.github.com>
2023-05-31 15:38:28 +0700

    Update quanlyhocvien.py

--- Clazz/quanlyhocvien.py
+++ Clazz/quanlyhocvien.py
@@ -27,7 +27,7 @@ class Quanlyhocvien:
                 hocvien.xeploai="Gioi"
             else:
                 hocvien.xeploai="Yeu"
-            hocvien.id=i+1
+            hocvien.id = self.list_hocvien.__len__() + 1
             self.PTTC1_list.append(hocvien)
 
     def hienthihocvien(self):
