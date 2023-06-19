b652055a2dd42e0ee230c0cdb38a5adb271cd153
lanleh <lanleh1612@gmail.com>
2023-06-19 14:55:25 +0700

    btvn sql

--- sql/Quanlyhocvien.py
+++ sql/Quanlyhocvien.py
@@ -30,12 +30,51 @@ def getalldata2():
         ketqua = dulieu.fetchone()
 
 def createdata():
-    dulieu.execute("INSERT INTO `Hocvien`(Id,`Name`,Age,Country,English,Information) VALUES ({},{},{},{},{},{})")
-    ketnoi.commit
-    print("Da them du lieu thanh cong")
+    print("Them du lieu")
+    id=int(input("Nhap id:"))
+    name=input("Nhap ten:")
+    age=int(input("Nhap tuoi:"))
+    country=input("Nhap que quan:")
+    english=int(input("Nhap diem tieng Anh:"))
+    info=int(input("Nhap diem tin hoc:"))
+    sql= "INSERT INTO `Hocvien`(Id,`Name`,Age,Country,English,Information) VALUES ({},'{}',{},'{}',{},{})".format(id,name,age,country,english,info)
+    dulieu.execute(sql)
+    ketnoi.commit()
+
+
+def update_data():
+    id=int(input("Nhap ID cua du lieu can sua:"))
+    print("Ban can sua thong tin gi? \n 1: Ten \n 2: Tuoi \n 3: Que quan \n 4: Diem tieng Anh \n 5: Diem tin hoc")
+    cau_lenh=int(input("Nhap 1,2,3,4:"))
+    if cau_lenh==1:
+        new_info=input("Nhap thong tin moi:")
+        dulieu.execute("UPDATE Quan_ly_hoc_vien.Hocvien SET `Name` = '{}' WHERE Id = {}".format(new_info,id))
+    elif cau_lenh==2:
+        new_info=int(input("Nhap thong tin moi:"))
+        dulieu.execute("UPDATE Quan_ly_hoc_vien.Hocvien SET Age = {} WHERE Id = {}".format(new_info,id))
+    elif cau_lenh==3:
+        new_info=input("Nhap thong tin moi:")
+        dulieu.execute("UPDATE Quan_ly_hoc_vien.Hocvien SET Country = '{}' WHERE Id = {}".format(new_info,id))
+    elif cau_lenh==4:
+        new_info=int(input("Nhap thong tin moi:"))
+        dulieu.execute("UPDATE Quan_ly_hoc_vien.Hocvien SET English = {} WHERE Id = {}".format(new_info,id))
+    elif cau_lenh==5:
+        new_info=int(input("Nhap thong tin moi:"))
+        dulieu.execute("UPDATE Quan_ly_hoc_vien.Hocvien SET Information = {} WHERE Id = {}".format(new_info,id))
+    ketnoi.commit()
+
+
+
 
 # Lay du lieu to Mysql ve Visual Code
-data_theo_id()
+# data_theo_id()
+
+# Them du lieu
+# createdata()
+
+# Update du lieu
+# update_data()
+
 
 # Dong ket noi
 ketnoi.close()
