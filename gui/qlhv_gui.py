c8cc474ab05cd862994e87176cd609dd513f6cef
lanleh <lanleh1612@gmail.com>
2023-07-08 15:44:51 +0700

    sua hv

--- gui/qlhv_gui.py
+++ gui/qlhv_gui.py
@@ -12,7 +12,7 @@ dulieu = ketnoi.cursor()
 # Tạo giao diện
 qlhv = Tk()
 qlhv.title = "Quản lý học viên"
-qlhv.geometry("400x400")
+qlhv.geometry("500x400")
 
 # Tiêu đề
 top = Label(qlhv, text = "CHƯƠNG TRÌNH QUẢN LÝ HỌC VIÊN", font=("Times New Roman","12")).grid(column = 2, row = 1)
@@ -49,7 +49,7 @@ eng_box = Entry(qlhv, width = 25, textvariable = giatri5).grid(column = 2, row =
 
 #Màn hình hiển thị
 thongtinhv = StringVar()
-scrolled = ScrolledText(qlhv,width= 25, height = 10).grid(column=2,row=9)
+scrolled = ScrolledText(qlhv,width= 25, height = 10).grid(column=2,row=10)
 
 
 # Thêm học viên
@@ -64,12 +64,39 @@ def themhocvien():
 
 them_hv = Button(qlhv,text = "Thêm học viên",command = themhocvien).grid(column = 1, row = 8)
 
+
 #Hiển thị học viên
 def hienthi():
     dulieu.execute("SELECT * FROM quan_ly_hoc_vien_2.hocvien")
     text = dulieu.fetchall()
     scrolled.insert(INSERT, text)
+    ketnoi.close()
 
 hienthi_hv = Button(qlhv, text = "Hiển thị học viên", command = hienthi).grid(column = 2, row = 8)
+
+
+#Sửa học viên
+def sua():
+    sua1 = Label(qlhv, text="ID của học viên cần sửa").grid(column = 1, row = 10)
+    id = IntVar()
+    id_box = Entry(qlhv, width = 25, textvariable = id).grid(column = 2, row = 10)
+    def thaotac():
+        dulieu.execute("SELECT * FROM quan_ly_hoc_vien_2.hocvien")
+        danh_sach = dulieu.fetchall()
+        for i in range(len(danh_sach)):
+            if id == danh_sach[i][0]:
+                print("yes")
+            else:
+                print("no")
+                #dulieu.execute("UPDATE quan_ly_hoc_vien_2.hocvien SET `Name` = '{}', Age = {}, Country = '{}', Sex = '{}', Information = {} and English = {} WHERE ID = {}".format(giatri1.get(),giatri2.get(),giatri3.get(),giatri6.get(),giatri4.get(),giatri5.get(),id))     
+                #ketnoi.commit()
+                #ketnoi.close()
+    nutthaotac = Button(qlhv, text = "Sửa", command = thaotac).grid(column = 3, row = 10)
+    
+
+sua_hv = Button(qlhv, text = "Sửa học viên", command = sua).grid(column = 1, row = 9)
+
+
+
 
 qlhv.mainloop()
